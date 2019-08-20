2413f370ffa6ea2dac129575ac13ca052e5a0d1b
SergeyBerez <sergeyberezovsky1981@gmail.com>
2019-08-20 22:00:49 +0300

    1

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
-console.log('привет');
+let li = document.querySelector('.login-link');
+li.addEventListener('click', function(e) {
+  e.preventDefault();
+  let modal = document.querySelector('.modal.modal-login ');
+  console.log(modal);
+  if (modal.style.display == 'block') {
+    modal.style.display = 'none';
+  } else {
+    modal.style.display = 'block';
+  }
+});
+
+let but = document.querySelector('.modal-close');
+but.addEventListener('click', function(e) {
+  let modal = (document.querySelector('.modal.modal-login').style.display =
+    'none');
+});
